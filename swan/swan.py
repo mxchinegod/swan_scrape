677001e4005691b233358b3f9286b8a1d1ffbb87
DylanAlloy <alloydylan@icloud.com>
2023-09-03 15:20:56 -0400

    🚧 fix(wip): destroy

--- swan/swan.py
+++ swan/swan.py
@@ -1,5 +1,5 @@
-from .utils import _f
-from os import path
+from .utils import _f, check
+import os, shutil
 from swan.config import Config
 from swan.copier import Copier
 from swan.receipts import Receipts
@@ -7,6 +7,7 @@ from swan.janitor import Janitor
 
 class Swan:
     def __init__(self, config: str | dict = None):
+        self.runs = []
         self.config = Config(config)
     def go(self):
         self.config.use()
@@ -20,4 +21,28 @@ class Swan:
         r.write()
         j.process(data)
         if self.config and copy and r and j:
-            return _f('success', '🦢 done')
+            _f('success', '🦢 done')
+            self.runs.append({
+                "config": self.config
+                , "copier": copy
+                , "receipts": r
+                , "janitor": j
+                , "data": data
+                , "status": 'complete'
+            })
+            return {
+                "config": self.config
+                , "copier": copy
+                , "receipts": r
+                , "janitor": j
+                , "data": data
+                , "status": 'complete'
+            }
+            # handle missing or broken objects in the runs
+    def destroy(self, confirm:str = None):
+        if not check(os.path.join(self.config.conf['settings']['proj_dir'], self.config.conf['settings']['name'])):
+            return _f('fatal', f'invalid path - {self.p}')
+        if confirm==self.config.conf["settings"]["name"]:
+            shutil.rmtree(os.path.join(self.config.conf['settings']['proj_dir'], self.config.conf['settings']['name'])), _f('warn', f'{confirm} destroyed')
+        else:
+            _f('fatal','you did not confirm - `Swan.destroy("your_config_name")`')
